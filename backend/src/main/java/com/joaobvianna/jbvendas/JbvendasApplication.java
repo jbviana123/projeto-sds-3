7d263a75ca093757c0425471a903154c4222b7ff
joao  batista viana <joaobvianna@hotmail.com>
2021-05-07 14:42:36 -0300

    projetos criados

---
+++ backend/src/main/java/com/joaobvianna/jbvendas/JbvendasApplication.java
@@ -0,0 +1,13 @@
+package com.joaobvianna.jbvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/joaobvianna/jbvendas/JbvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaobvianna.jbvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
